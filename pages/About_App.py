114821ce17d85412d9b1afde04945668f25c664b
Dhikrullah Muhammad <muhammaddhikrullah59@gmail.com>
2023-03-07 10:52:55 +0100

    Added new info on home page again and added new page called the About App.

---
+++ pages/About_App.py
@@ -0,0 +1,55 @@
+import streamlit as st
+
+
+st.write("# About App")
+st.text(
+    """
+    The name of the app is "Top-G To-Do List".
+    It is basically for adding and deleting to-do. 
+
+    """
+)
+
+st.text(
+    """
+    This is basically a to-do app where you can add list of things you 
+    wish to accomplish in a day or two so, you basically do that by
+    adding your to-do in the "add new to-do" box, press enter then write 
+    a full detailed note on your to-do in "notes" below the list of to-do box and then press enter. 
+    """
+)
+
+st.text(
+    """
+        👈 You can always read details on your to-do using the sidebar to navigate to the
+        "My To-Do Documentation" page.    
+
+    """
+)
+
+st.text(
+    """ 
+    The "My To-Do Documentation" page contains your to-do as an header 
+    and your to-do notes as a sub-header for you to read anytime.
+    
+    """
+)
+
+st.text(
+    """
+    When you click on the check box in the home page, it will 
+    make the to-do you click on move to the "Completed To-Do" tab.
+     
+     
+    """
+ )
+
+st.text(
+    """
+    When you click on the check box in the home page, it will automatically
+    delete the to-do finally from the list of your to-do which means you 
+    have done that particular todo. 
+ 
+    """
+)
+
